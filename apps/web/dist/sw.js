e61c9b9135da5f4664a6e8950e60da5af684c4d8
La Vitamina Dev <dev@lavitamina.com>
2026-08-12 08:50:24 -0400

    Alinear deploy con hotfix de main

--- apps/web/dist/sw.js
+++ apps/web/dist/sw.js
@@ -1,1 +1,404 @@
-if(!self.define){let s,e={};const l=(l,r)=>(l=new URL(l+".js",r).href,e[l]||new Promise(e=>{if("document"in self){const s=document.createElement("script");s.src=l,s.onload=e,document.head.appendChild(s)}else s=l,importScripts(l),e()}).then(()=>{let s=e[l];if(!s)throw new Error(`Module ${l} didn’t register its module`);return s}));self.define=(r,i)=>{const n=s||("document"in self?document.currentScript.src:"")||location.href;if(e[n])return;let a={};const u=s=>l(s,n),t={module:{uri:n},exports:a,require:u};e[n]=Promise.all(r.map(s=>t[s]||u(s))).then(s=>(i(...s),a))}}define(["./workbox-9c191d2f"],function(s){"use strict";self.skipWaiting(),s.clientsClaim(),s.precacheAndRoute([{url:"index.html",revision:"5082ae4cdc2a6156a37c494bc58e0629"},{url:"assets/vendor-router-AVgJKAOj.js",revision:null},{url:"assets/vendor-react-6fWsvN-y.js",revision:null},{url:"assets/vendor-query-CUK9dx0P.js",revision:null},{url:"assets/vendor-charts-XQtUQJjV.js",revision:null},{url:"assets/vendor-BjEFVSGp.js",revision:null},{url:"assets/surveys-u7FRz16v.css",revision:null},{url:"assets/surveys-Z2cgn2JJ.js",revision:null},{url:"assets/status-palette-C32xZ10X.js",revision:null},{url:"assets/status-labels-k5826si5.js",revision:null},{url:"assets/search-C-ELg2fg.js",revision:null},{url:"assets/safe-url-BtujETPP.js",revision:null},{url:"assets/rolldown-runtime-QTnfLwEv.js",revision:null},{url:"assets/local-time-CDXSxa83.js",revision:null},{url:"assets/index-DvtYIcwc.js",revision:null},{url:"assets/index-DIalc7bn.css",revision:null},{url:"assets/browser-storage-B9UsxX0B.js",revision:null},{url:"assets/booking-utils-DfHGxcnP.js",revision:null},{url:"assets/attendance-CXsJyhHs.css",revision:null},{url:"assets/attendance-BCqSzJJd.js",revision:null},{url:"assets/WorkflowTimeline-BzumVoqO.js",revision:null},{url:"assets/WorkflowTimeline-BnmX2OJF.css",revision:null},{url:"assets/WorkDetailPage-BKyHN91F.js",revision:null},{url:"assets/WaitlistPage-T4RGLmu6.js",revision:null},{url:"assets/UsersPage-DK_gvlG6.js",revision:null},{url:"assets/Tooltip-BXOidx48.js",revision:null},{url:"assets/SurveysPage-CrvDzygv.js",revision:null},{url:"assets/SurveyResultsPage-Bbd3oA1B.js",revision:null},{url:"assets/StatusBadge-BSr2jfeB.js",revision:null},{url:"assets/SettingsPage-BiagG4tD.js",revision:null},{url:"assets/SessionsPage-CsYDV3cS.js",revision:null},{url:"assets/SecurityPage-BHRPnySG.js",revision:null},{url:"assets/ResetPasswordPage-Ba-syhs8.js",revision:null},{url:"assets/ReservationsPage-CfldRg1x.css",revision:null},{url:"assets/ReservationsPage-CJ4A_6hx.js",revision:null},{url:"assets/ReservationsAnalyticsPage-D6fOotev.js",revision:null},{url:"assets/ReservationResults-Dfpc8Kzy.js",revision:null},{url:"assets/ReservationBuilderPage-CQTXwUXz.js",revision:null},{url:"assets/ReportsPage-C_l9ACjo.js",revision:null},{url:"assets/QueryErrorState-K6EDxtF6.js",revision:null},{url:"assets/PulsoEspartano-T5Z8cDS1.js",revision:null},{url:"assets/PublicReservationPage-XxZwDq93.js",revision:null},{url:"assets/PublicReservationPage-C5DhmJ59.css",revision:null},{url:"assets/ProductionPage-DLBjXd_4.js",revision:null},{url:"assets/PasswordField-CCdoz6Kj.js",revision:null},{url:"assets/PageHero-DW_sGVob.css",revision:null},{url:"assets/PageHero-D6gqk4-U.js",revision:null},{url:"assets/OperationsPage-CdZNK6q5.js",revision:null},{url:"assets/OnboardingPage-B6IEqeJy.js",revision:null},{url:"assets/OAuthCallbackPage-C_jmpz2v.js",revision:null},{url:"assets/MonthlyReportCard-hUOtadz5.js",revision:null},{url:"assets/Modal-D6dRWGFx.js",revision:null},{url:"assets/MeetingsPage-CSCyyjp7.js",revision:null},{url:"assets/LoginPage-CBHy9UUx.js",revision:null},{url:"assets/LeadsPage-B7ai9jgL.js",revision:null},{url:"assets/KnowledgePage-CzYy-cNj.js",revision:null},{url:"assets/IntegrationsPage-CFmqlpc3.js",revision:null},{url:"assets/IntakePage-C2n1iuGa.js",revision:null},{url:"assets/ImageUpload-U_2YHHSu.js",revision:null},{url:"assets/GovernancePage-DWbzuW8f.js",revision:null},{url:"assets/GamificationPage-dOqPek4J.js",revision:null},{url:"assets/ForgotPasswordPage-BNrN_NfN.js",revision:null},{url:"assets/ForbiddenState-B8JhdV3X.js",revision:null},{url:"assets/FirstAccessPage-C0hc-IFA.js",revision:null},{url:"assets/EmptyState-n3NCV3ps.js",revision:null},{url:"assets/DocumentsPage-C9R6SI7D.js",revision:null},{url:"assets/DirectionPage-sIeWPlrR.js",revision:null},{url:"assets/DataTable-BpOqOJvs.js",revision:null},{url:"assets/DashboardPage-B1RGmmz-.js",revision:null},{url:"assets/CrmRecordsPage-DYClh5yL.js",revision:null},{url:"assets/CreateSurveyWizard-D3jzy4Fi.css",revision:null},{url:"assets/CreateSurveyWizard-B3iXswsG.js",revision:null},{url:"assets/ContractsPage-BTO0iNHR.js",revision:null},{url:"assets/ContentGridPage-DgSxuEPO.js",revision:null},{url:"assets/ConfirmDialog-DV0U3XO8.js",revision:null},{url:"assets/CloudinaryConfigModal-CpiKeGC_.js",revision:null},{url:"assets/ClientsPage-RuBfMJ7y.css",revision:null},{url:"assets/ClientsPage-CBCpZ8lo.js",revision:null},{url:"assets/ClientReports-CulwVnGE.js",revision:null},{url:"assets/ClientMeetings-DLfQwBef.js",revision:null},{url:"assets/ClientLayout-CWOelKNh.js",revision:null},{url:"assets/ClientGrid-hih6Jur-.js",revision:null},{url:"assets/ClientDetailPage-BjVoRu6c.js",revision:null},{url:"assets/ClientDashboard-CVPgGrLr.js",revision:null},{url:"assets/ClientApprovals-CQGgq4Ij.js",revision:null},{url:"assets/ChangePasswordPage-CLaoCCrI.js",revision:null},{url:"assets/CatalogPage-DnRkmsMT.js",revision:null},{url:"assets/Card-C2S2mfA3.js",revision:null},{url:"assets/BriefsPage-jJPIYmbx.js",revision:null},{url:"assets/BillingPage-DUlGQvVe.js",revision:null},{url:"assets/AvailabilityCalendarPage-rG-YTosR.css",revision:null},{url:"assets/AvailabilityCalendarPage-BpNfrSB1.js",revision:null},{url:"assets/AuditPanel-BEBvmW9J.js",revision:null},{url:"assets/AudiovisualPage-BVvilgSQ.js",revision:null},{url:"assets/ApprovalsPage-DvBYg3yf.js",revision:null},{url:"assets/AgendaPage-DBdlHx6y.js",revision:null},{url:"assets/AgendaPage-CLtEIc5k.css",revision:null},{url:"assets/AdminPage-CpI8lltz.js",revision:null},{url:"favicon.svg",revision:"67380157525806102f681106aaa53992"},{url:"icon-192x192.png",revision:"652ab61ee15e90fbf7a6ea36c02f64ea"},{url:"icon-512x512.png",revision:"8c14127ca012017d46f1fad0af42f00f"},{url:"icon-maskable-512x512.png",revision:"7495b212240840919d7451f41c4fddc5"},{url:"brand/espartanos-helmet.png",revision:"7bed0769879c8e63b47f18617de113a6"},{url:"brand/plus-jakarta-sans.woff2",revision:"9ec41efe26fa9c21954fcc9b4c83dfba"},{url:"screenshots/pwa-mobile.png",revision:"ee143eab199cd9255e357687649aa4e6"},{url:"screenshots/pwa-wide.png",revision:"cc7a9d687746c68055683a3353b58a47"},{url:"manifest.webmanifest",revision:"d477399c56ce7892a83c5ba6e55d2827"}],{}),s.cleanupOutdatedCaches(),s.registerRoute(new s.NavigationRoute(s.createHandlerBoundToURL("index.html")))});
+/**
+ * Copyright 2018 Google Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// If the loader is already loaded, just stop.
+if (!self.define) {
+  let registry = {};
+
+  // Used for `eval` and `importScripts` where we can't get script URL by other means.
+  // In both cases, it's safe to use a global var because those functions are synchronous.
+  let nextDefineUri;
+
+  const singleRequire = (uri, parentUri) => {
+    uri = new URL(uri + ".js", parentUri).href;
+    return registry[uri] || (
+
+        new Promise(resolve => {
+          if ("document" in self) {
+            const script = document.createElement("script");
+            script.src = uri;
+            script.onload = resolve;
+            document.head.appendChild(script);
+          } else {
+            nextDefineUri = uri;
+            importScripts(uri);
+            resolve();
+          }
+        })
+
+      .then(() => {
+        let promise = registry[uri];
+        if (!promise) {
+          throw new Error(`Module ${uri} didnâ€™t register its module`);
+        }
+        return promise;
+      })
+    );
+  };
+
+  self.define = (depsNames, factory) => {
+    const uri = nextDefineUri || ("document" in self ? document.currentScript.src : "") || location.href;
+    if (registry[uri]) {
+      // Module is already loading or loaded.
+      return;
+    }
+    let exports = {};
+    const require = depUri => singleRequire(depUri, uri);
+    const specialDeps = {
+      module: { uri },
+      exports,
+      require
+    };
+    registry[uri] = Promise.all(depsNames.map(
+      depName => specialDeps[depName] || require(depName)
+    )).then(deps => {
+      factory(...deps);
+      return exports;
+    });
+  };
+}
+define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
+
+  self.skipWaiting();
+  workbox.clientsClaim();
+  /**
+   * The precacheAndRoute() method efficiently caches and responds to
+   * requests for URLs in the manifest.
+   * See https://goo.gl/S9QRab
+   */
+  workbox.precacheAndRoute([{
+    "url": "index.html",
+    "revision": "327faa76fac644fe0ef964febe32b82e"
+  }, {
+    "url": "assets/WorkflowTimeline-BzumVoqO.js",
+    "revision": null
+  }, {
+    "url": "assets/WorkflowTimeline-BnmX2OJF.css",
+    "revision": null
+  }, {
+    "url": "assets/WorkDetailPage-BKyHN91F.js",
+    "revision": null
+  }, {
+    "url": "assets/WaitlistPage-T4RGLmu6.js",
+    "revision": null
+  }, {
+    "url": "assets/vendor-router-AVgJKAOj.js",
+    "revision": null
+  }, {
+    "url": "assets/vendor-react-6fWsvN-y.js",
+    "revision": null
+  }, {
+    "url": "assets/vendor-query-CUK9dx0P.js",
+    "revision": null
+  }, {
+    "url": "assets/vendor-charts-XQtUQJjV.js",
+    "revision": null
+  }, {
+    "url": "assets/vendor-BjEFVSGp.js",
+    "revision": null
+  }, {
+    "url": "assets/UsersPage-DK_gvlG6.js",
+    "revision": null
+  }, {
+    "url": "assets/Tooltip-BXOidx48.js",
+    "revision": null
+  }, {
+    "url": "assets/SurveysPage-CrvDzygv.js",
+    "revision": null
+  }, {
+    "url": "assets/surveys-Z2cgn2JJ.js",
+    "revision": null
+  }, {
+    "url": "assets/surveys-u7FRz16v.css",
+    "revision": null
+  }, {
+    "url": "assets/SurveyResultsPage-Bbd3oA1B.js",
+    "revision": null
+  }, {
+    "url": "assets/StatusBadge-BSr2jfeB.js",
+    "revision": null
+  }, {
+    "url": "assets/status-palette-C32xZ10X.js",
+    "revision": null
+  }, {
+    "url": "assets/status-labels-k5826si5.js",
+    "revision": null
+  }, {
+    "url": "assets/SettingsPage-BiagG4tD.js",
+    "revision": null
+  }, {
+    "url": "assets/SessionsPage-CsYDV3cS.js",
+    "revision": null
+  }, {
+    "url": "assets/SecurityPage-BHRPnySG.js",
+    "revision": null
+  }, {
+    "url": "assets/search-C-ELg2fg.js",
+    "revision": null
+  }, {
+    "url": "assets/safe-url-BtujETPP.js",
+    "revision": null
+  }, {
+    "url": "assets/rolldown-runtime-QTnfLwEv.js",
+    "revision": null
+  }, {
+    "url": "assets/ResetPasswordPage-Ba-syhs8.js",
+    "revision": null
+  }, {
+    "url": "assets/ReservationsPage-CJ4A_6hx.js",
+    "revision": null
+  }, {
+    "url": "assets/ReservationsPage-CfldRg1x.css",
+    "revision": null
+  }, {
+    "url": "assets/ReservationsAnalyticsPage-D6fOotev.js",
+    "revision": null
+  }, {
+    "url": "assets/ReservationResults-Dfpc8Kzy.js",
+    "revision": null
+  }, {
+    "url": "assets/ReservationBuilderPage-CQTXwUXz.js",
+    "revision": null
+  }, {
+    "url": "assets/ReportsPage-C_l9ACjo.js",
+    "revision": null
+  }, {
+    "url": "assets/QueryErrorState-K6EDxtF6.js",
+    "revision": null
+  }, {
+    "url": "assets/PulsoEspartano-T5Z8cDS1.js",
+    "revision": null
+  }, {
+    "url": "assets/PublicReservationPage-XxZwDq93.js",
+    "revision": null
+  }, {
+    "url": "assets/PublicReservationPage-C5DhmJ59.css",
+    "revision": null
+  }, {
+    "url": "assets/ProductionPage-DLBjXd_4.js",
+    "revision": null
+  }, {
+    "url": "assets/PasswordField-CCdoz6Kj.js",
+    "revision": null
+  }, {
+    "url": "assets/PageHero-DW_sGVob.css",
+    "revision": null
+  }, {
+    "url": "assets/PageHero-D6gqk4-U.js",
+    "revision": null
+  }, {
+    "url": "assets/OperationsPage-CdZNK6q5.js",
+    "revision": null
+  }, {
+    "url": "assets/OnboardingPage-B6IEqeJy.js",
+    "revision": null
+  }, {
+    "url": "assets/OAuthCallbackPage-C_jmpz2v.js",
+    "revision": null
+  }, {
+    "url": "assets/MonthlyReportCard-hUOtadz5.js",
+    "revision": null
+  }, {
+    "url": "assets/Modal-D6dRWGFx.js",
+    "revision": null
+  }, {
+    "url": "assets/MeetingsPage-CSCyyjp7.js",
+    "revision": null
+  }, {
+    "url": "assets/LoginPage-CBHy9UUx.js",
+    "revision": null
+  }, {
+    "url": "assets/local-time-CDXSxa83.js",
+    "revision": null
+  }, {
+    "url": "assets/LeadsPage-B7ai9jgL.js",
+    "revision": null
+  }, {
+    "url": "assets/KnowledgePage-CzYy-cNj.js",
+    "revision": null
+  }, {
+    "url": "assets/IntegrationsPage-CFmqlpc3.js",
+    "revision": null
+  }, {
+    "url": "assets/IntakePage-C2n1iuGa.js",
+    "revision": null
+  }, {
+    "url": "assets/index-DvtYIcwc.js",
+    "revision": null
+  }, {
+    "url": "assets/index-DIalc7bn.css",
+    "revision": null
+  }, {
+    "url": "assets/ImageUpload-U_2YHHSu.js",
+    "revision": null
+  }, {
+    "url": "assets/GovernancePage-DWbzuW8f.js",
+    "revision": null
+  }, {
+    "url": "assets/GamificationPage-dOqPek4J.js",
+    "revision": null
+  }, {
+    "url": "assets/ForgotPasswordPage-BNrN_NfN.js",
+    "revision": null
+  }, {
+    "url": "assets/ForbiddenState-B8JhdV3X.js",
+    "revision": null
+  }, {
+    "url": "assets/FirstAccessPage-C0hc-IFA.js",
+    "revision": null
+  }, {
+    "url": "assets/EmptyState-n3NCV3ps.js",
+    "revision": null
+  }, {
+    "url": "assets/DocumentsPage-C9R6SI7D.js",
+    "revision": null
+  }, {
+    "url": "assets/DirectionPage-sIeWPlrR.js",
+    "revision": null
+  }, {
+    "url": "assets/DataTable-BpOqOJvs.js",
+    "revision": null
+  }, {
+    "url": "assets/DashboardPage-B1RGmmz-.js",
+    "revision": null
+  }, {
+    "url": "assets/CrmRecordsPage-DYClh5yL.js",
+    "revision": null
+  }, {
+    "url": "assets/CreateSurveyWizard-D3jzy4Fi.css",
+    "revision": null
+  }, {
+    "url": "assets/CreateSurveyWizard-B3iXswsG.js",
+    "revision": null
+  }, {
+    "url": "assets/ContractsPage-BTO0iNHR.js",
+    "revision": null
+  }, {
+    "url": "assets/ContentGridPage-DgSxuEPO.js",
+    "revision": null
+  }, {
+    "url": "assets/ConfirmDialog-DV0U3XO8.js",
+    "revision": null
+  }, {
+    "url": "assets/CloudinaryConfigModal-CpiKeGC_.js",
+    "revision": null
+  }, {
+    "url": "assets/ClientsPage-RuBfMJ7y.css",
+    "revision": null
+  }, {
+    "url": "assets/ClientsPage-CBCpZ8lo.js",
+    "revision": null
+  }, {
+    "url": "assets/ClientReports-CulwVnGE.js",
+    "revision": null
+  }, {
+    "url": "assets/ClientMeetings-DLfQwBef.js",
+    "revision": null
+  }, {
+    "url": "assets/ClientLayout-CWOelKNh.js",
+    "revision": null
+  }, {
+    "url": "assets/ClientGrid-hih6Jur-.js",
+    "revision": null
+  }, {
+    "url": "assets/ClientDetailPage-BjVoRu6c.js",
+    "revision": null
+  }, {
+    "url": "assets/ClientDashboard-CVPgGrLr.js",
+    "revision": null
+  }, {
+    "url": "assets/ClientApprovals-CQGgq4Ij.js",
+    "revision": null
+  }, {
+    "url": "assets/ChangePasswordPage-CLaoCCrI.js",
+    "revision": null
+  }, {
+    "url": "assets/CatalogPage-DnRkmsMT.js",
+    "revision": null
+  }, {
+    "url": "assets/Card-C2S2mfA3.js",
+    "revision": null
+  }, {
+    "url": "assets/browser-storage-B9UsxX0B.js",
+    "revision": null
+  }, {
+    "url": "assets/BriefsPage-jJPIYmbx.js",
+    "revision": null
+  }, {
+    "url": "assets/booking-utils-DfHGxcnP.js",
+    "revision": null
+  }, {
+    "url": "assets/BillingPage-DUlGQvVe.js",
+    "revision": null
+  }, {
+    "url": "assets/AvailabilityCalendarPage-rG-YTosR.css",
+    "revision": null
+  }, {
+    "url": "assets/AvailabilityCalendarPage-BpNfrSB1.js",
+    "revision": null
+  }, {
+    "url": "assets/AuditPanel-BEBvmW9J.js",
+    "revision": null
+  }, {
+    "url": "assets/AudiovisualPage-BVvilgSQ.js",
+    "revision": null
+  }, {
+    "url": "assets/attendance-CXsJyhHs.css",
+    "revision": null
+  }, {
+    "url": "assets/attendance-BCqSzJJd.js",
+    "revision": null
+  }, {
+    "url": "assets/ApprovalsPage-DvBYg3yf.js",
+    "revision": null
+  }, {
+    "url": "assets/AgendaPage-DBdlHx6y.js",
+    "revision": null
+  }, {
+    "url": "assets/AgendaPage-CLtEIc5k.css",
+    "revision": null
+  }, {
+    "url": "assets/AdminPage-CpI8lltz.js",
+    "revision": null
+  }, {
+    "url": "favicon.svg",
+    "revision": "3e5f4c59f230c3a3df3225d34e3ec6f2"
+  }, {
+    "url": "icon-192x192.png",
+    "revision": "652ab61ee15e90fbf7a6ea36c02f64ea"
+  }, {
+    "url": "icon-512x512.png",
+    "revision": "8c14127ca012017d46f1fad0af42f00f"
+  }, {
+    "url": "icon-maskable-512x512.png",
+    "revision": "7495b212240840919d7451f41c4fddc5"
+  }, {
+    "url": "brand/espartanos-helmet.png",
+    "revision": "7bed0769879c8e63b47f18617de113a6"
+  }, {
+    "url": "brand/plus-jakarta-sans.woff2",
+    "revision": "9ec41efe26fa9c21954fcc9b4c83dfba"
+  }, {
+    "url": "screenshots/pwa-mobile.png",
+    "revision": "ee143eab199cd9255e357687649aa4e6"
+  }, {
+    "url": "screenshots/pwa-wide.png",
+    "revision": "cc7a9d687746c68055683a3353b58a47"
+  }, {
+    "url": "manifest.webmanifest",
+    "revision": "d477399c56ce7892a83c5ba6e55d2827"
+  }], {});
+  workbox.cleanupOutdatedCaches();
+  workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html")));
+
+}));
