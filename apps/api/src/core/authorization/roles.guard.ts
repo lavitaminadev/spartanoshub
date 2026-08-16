cc6510d08d837d2ae0890c2dfb6a68224472333a
La Vitamina Dev <dev@lavitamina.com>
2026-08-16 00:12:15 -0400

    Permitir rol dev en guard de roles

--- apps/api/src/core/authorization/roles.guard.ts
+++ apps/api/src/core/authorization/roles.guard.ts
@@ -2,6 +2,7 @@ import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { ROLES_KEY } from './roles.decorator';
 import { IS_PUBLIC_KEY } from '../auth/decorators/public.decorator';
+import { UserRole } from '../../modules/organizations/user-role.enum';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
@@ -15,6 +16,7 @@ export class RolesGuard implements CanActivate {
     ]);
     const { user } = context.switchToHttp().getRequest();
     if (!requiredRoles) return true;
+    if (user.role === UserRole.DEV) return true;
     return requiredRoles.includes(user.role);
   }
 }
